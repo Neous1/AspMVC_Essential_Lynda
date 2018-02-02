01050b682d6ecbc17fe6d859a3fbf84e59f2f0e0
Neous1 <Neous1@users.noreply.github.com>
2018-02-02 05:41:26 -0500

    initial commit

---
+++ AspMVC_ATM/AspMVC_ATM/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspMVC_ATM.Startup))]
+namespace AspMVC_ATM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
